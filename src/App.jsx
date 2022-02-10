1f9fbd1e985bd596934ef59c7a6fa7dcdd958446
Abhiraj <codersground404@gmail.com>
2022-02-10 21:47:39 +0530

    Hello page to loadscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { Main } from "./pages/Main/Main";
 import { Blog } from "./pages/Blog/Blog";
 import { Skills } from "./pages/Skills/Skills";
 import { About } from "./pages/About/About";
 import GlobalStyle from "./Global/globalstyles";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import { Work } from "./pages/Work/Work";
+import { AnimatePresence } from "framer-motion";
+
 export const App = () => {
+  const [loading, setLoding] = useState(false);
+  useEffect(() => {
+    setLoding(true);
+    setTimeout(() => {
+      setLoding(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
       <GlobalStyle />
-
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Main />} />
-          <Route exact path="/Home" element={<HomePage />} />
-          <Route exact path="/Blog" element={<Blog />} />
-          <Route exact path="/Skills" element={<Skills />} />
-          <Route exact path="/About" element={<About />} />
-          <Route exact path="/Work" element={<Work />} />
-        </Routes>
-      </Router>
+      {loading ? (
+        <HomePage />
+      ) : (
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route exact path="/Blog" element={<Blog />} />
+            <Route exact path="/Skills" element={<Skills />} />
+            <Route exact path="/About" element={<About />} />
+            <Route exact path="/Work" element={<Work />} />
+          </Routes>
+        </Router>
+      )}
     </>
   );
 };
